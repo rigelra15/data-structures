4d27903dcd319dbb821fe16b124602bc7d0a1665
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2023-09-06 13:20:28 +0700

    Fix

--- Numbers/arrayNumbers.py
+++ Numbers/arrayNumbers.py
@@ -6,20 +6,22 @@ Created on Mon Sep  4 08:31:24 2023
 """
 
 class NumbersList:
+    # 1. Initiate for the Array
     def __init__(self, ArrayLength=100):
         self.Value = [0] * ArrayLength
         self.TopIndex = -1
         self.MaxIndex = ArrayLength - 1
 
+    # 2. Function for Add Number
     def Append(self, Value):
         if self.TopIndex < self.MaxIndex:
             self.TopIndex = self.TopIndex + 1
             self.Value[self.TopIndex] = Value
 
+    # 3. Print the List
     def Print(self):
         if self.TopIndex == -1:
             print("[] - The list is empty.")
-            print("")
             return
         print("[", end='')
         for i in range(self.TopIndex + 1):
@@ -28,6 +30,7 @@ class NumbersList:
                 print(", ", end='')
         print("]")
 
+    # 4. Search the Index of the Number
     def Search(self, Value):
         SearchIndex = -1
         for i in range(self.TopIndex + 1):
@@ -36,6 +39,7 @@ class NumbersList:
                 break
         return SearchIndex
 
+    # 5. Delete Number
     def Delete(self, DeleteIndex):
         if self.TopIndex == -1:
             print("The list is empty. No numbers to delete.")
@@ -49,46 +53,64 @@ class NumbersList:
             self.Value[i] = self.Value[i + 1]
         self.TopIndex = self.TopIndex - 1
         
+    # 6. Check the List (if still there is number or not)
     def CheckList(self):
         if self.TopIndex == -1:
             return 1
+    
+    # 7. Reset the List
+    def Reset(self):
+        self.TopIndex = -1
+        self.Value = [0] * (self.MaxIndex + 1)
 
+##############
+# MAIN PROGRAM
+##############
 a = NumbersList(100)
 selectMenu = 1
 
-print("[LIST OF NUMBERS] - Using Array")
+print("\033[93m[LIST OF NUMBERS] - Using Array\033[0m\n")
 
-inputMany = input("Type the desired number of numbers: ")
-for i in range(int(inputMany)):
-    inputNum = input("Type number: ")
-    a.Append(int(inputNum))
-
-while selectMenu != 2:
-    print("")
+while selectMenu != 4:
     a.Print()
 
     print("Menu:")
-    print("1. Delete a Number")
-    print("2. Exit")
+    print("1. Delete Number")
+    print("2. Add Number")
+    print("3. Reset List")
+    print("4. Exit")
 
     selectMenu = input("Select: ")
     selectMenu = int(selectMenu)
 
+    # DELETE NUMBER
     if selectMenu == 1:
         if a.CheckList() == 1:
-            print("Sorry, but there are no more numbers to delete.")
+            print("Sorry, but there are no more numbers to delete.\n")
         else:
             print("")
             a.Print()
             while True:
-                selectNum = input("Select the number you want to delete: ")
+                selectNum = input("Type the number you want to delete: ")
                 selectedNum = a.Search(int(selectNum))
     
                 if selectedNum != -1:
                     a.Delete(selectedNum)
-                    print("Number DELETED!")
+                    print("\033[91mNumber DELETED!\033[0m") 
                     break
     
-                print("Number NOT FOUND!")
+                print("\033[91mNumber NOT FOUND!\033[0m") 
+
+    # ADD NUMBER
+    elif selectMenu == 2:
+        addManyNum = input("How many numbers do you want to add?: ")
+        for i in range(int(addManyNum)):
+            addNum = input(f"Type number ({i+1}): ")
+            a.Append(int(addNum))
+
+    # RESET LIST
+    elif selectMenu == 3:
+        a.Reset()
+        print("\033[91mThe list has been reset!\033[0m\n")
 
 print("Thanks for trying!")
--- Numbers/listNumbers.py
+++ Numbers/listNumbers.py
@@ -7,63 +7,96 @@ Created on Mon Sep  4 09:35:18 2023
 """
 
 class NumbersList:
+    # 1. Initiate for the List
     def __init__(self):
         self.Values = list()
 
+    # 2. Function for Add Number
     def Append(self, Value):
         self.Values.append(Value)
 
+    # 3. Print the List
     def Print(self):
         if len(self.Values) == 0:
-            print(self.Values, end='')
-            print(" - The list is empty.")
+            print(str(self.Values) + " - The list is empty.")
         else:
             print(self.Values)
+    
+    # def Search(self, Value):
+    #     try:
+    #         return self.Values.index(Value)
+    #     except ValueError:
+    #         return -1
 
+    # 4. Delete Number
     def Delete(self, DeleteNumber):
         if DeleteNumber in self.Values:
             self.Values.remove(DeleteNumber)
             return True
         else:
             return False
+        
+        # if you use this, add function Search for find the Index
+        # if 0 <= DeleteIndex < len(self.Values):
+        #   del self.Values[DeleteIndex]
             
+    # 5. Check the List (if still there is number or not)
     def CheckList(self):
         if len(self.Values) == 0:
             return 1
+        
+    # 6. Reset the List
+    def Reset(self):
+        self.Values.clear()
 
 a = NumbersList()
 selectMenu = 1
 
-print("[LIST OF NUMBERS] - Using List")
+print("\033[93m[LIST OF NUMBERS] - Using List\033[0m\n")
 
-inputMany = input("Type the desired number of numbers: ")
-for _ in range(int(inputMany)):
-    inputNum = input("Ketik angka: ")
-    a.Append(int(inputNum))
+# inputMany = input("Type the desired number of numbers: ")
+# for _ in range(int(inputMany)):
+#     inputNum = input("Type number: ")
+#     a.Append(int(inputNum))
 
-while selectMenu != 2:
-    print("")
+while selectMenu != 4:
     a.Print()
 
     print("Menu:")
-    print("1. Delete a Number")
-    print("2. Exit")
+    print("1. Delete Number")
+    print("2. Add Number")
+    print("3. Reset List")
+    print("4. Exit")
     
     selectMenu = input("Select: ")
     selectMenu = int(selectMenu)
 
+    # DELETE NUMBER
     if selectMenu == 1:
         if a.CheckList() == 1:
-            print("Sorry, but there are no more numbers to delete")
+            print("Sorry, but there are no more numbers to delete.\n")
         else:
             print("")
             a.Print()
             while True:
-                selectNum = input("Select the number you want to delete: ")
+                selectNum = input("Type the number you want to delete: ")
                 if a.Delete(int(selectNum)):
-                    print("Number DELETED!")
+                    print("\033[91mNumber DELETED!\033[0m") 
                     break
                 
-                print("Number NOT FOUND!")
+                print("\033[91mNumber NOT FOUND!\033[0m") 
+
+    # ADD NUMBER
+    elif selectMenu == 2:
+        addManyNum = input("How many numbers do you want to add?: ")
+        for i in range(int(addManyNum)):
+            addNum = input(f"Type number ({i+1}): ")
+            a.Append(int(addNum))
+    
+    # RESET LIST
+    elif selectMenu == 3:
+        a.Reset()
+        print("\033[91mThe list has been reset!\033[0m\n")
+        
 
 print("Thanks for trying!")
